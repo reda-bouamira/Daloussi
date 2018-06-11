3ae0e8248ea7a3f15ae9c5494154b1adee3f8a06
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-11 07:54:42 -0500

    Revert "Revert "Initial commit""
    This reverts commit 24926a95ac70b15c003c70fdf780e1ecfd12b11c.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    	<?php include 'php/head.php' ?>
+    </head>
+    <body>
+        <header>
+            <?php include 'php/header.php' ?>
+        </header>
+
+        <!-- CarType: coupe sedan SUV truck van wagon -->
+        <footer>
+            <!-- <?php include 'php/footer.php'; ?> -->
+        </footer>
+    	<?php include 'php/scripts.php' ?>
+    </body>
+</html>
---
+++ php/main_nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid pb-0 pt-0">
+  <!-- <a class="navbar-brand" href="#">Home</a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active ml-4">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <div class="underline"></div>
+      </li>
+      <li class="nav-item ml-4">
+        <a class="nav-link" href="#">Used Vehicles</a>
+        <div class="underline"></div>
+      </li>
+      <li class="nav-item ml-4">
+        <a class="nav-link" href="#">Finance</a>
+        <div class="underline"></div>
+      </li>
+      <li class="nav-item ml-4">
+        <a class="nav-link" href="#">Service</a>
+        <div class="underline"></div>
+      </li>
+      <li class="nav-item ml-4">
+        <a class="nav-link" href="#">About Us</a>
+        <div class="underline"></div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
