31e218102bafb4a3c297b17fbf647480662d392e
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-11 07:52:37 -0500

    Initial commit

---
+++ php/header.php
@@ -0,0 +1,20 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3">
+			<img src="img/logo.png" class="img-fluid" alt="logo">
+		</div>	
+		<div class="offset-sm-5 col-sm-3 pt-2 align-self-center">
+			<ul class="social-icons pl-0 pb-0 mb-0">
+  		   		<li><a href="#" target="_blank" class="fa fa-facebook mr-2"></a></li>
+  		   		<li><a href="#" class="fa fa-twitter mr-2"></a></li>
+		   		<li><a href="#" class="fa fa-instagram mr-2"></a></li>
+		   		<li><a href="#" class="fa fa-envelope"></a></li>
+	    	</ul>
+			<div class="my_header_address"><a href="https://www.google.ca/maps/place/Daloussi+Auto/@49.8829714,-97.1118464,17z/data=!3m1!4b1!4m5!3m4!1s0x52ea76cd2dbc5f29:0xf42523ac8c67d856!8m2!3d49.882968!4d-97.1096577"><img src="img/gps_icon.png" alt="GPS" class="my_gps_icon">395 Goulet Street, Winnipeg, MB R2H 3C7</a></div>
+			<div><span class="font-weight-bold mr-2 pl-3">Call Us: </span>(204) 583-4617</div>
+		</div>
+	</div>
+	<div class="row bg-dark">
+		<?php include "php/main_nav.php" ?>
+	</div>
+</div>
