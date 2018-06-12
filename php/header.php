3def2e9b39581ec9b7e325d2804aa046e0292bda
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-12 09:12:23 -0500

    Fixed Navbar & New Tab for Google Maps
    added the .navbar-fixed-top so that the nav-bar sticks to the top.

--- php/header.php
+++ php/header.php
@@ -10,7 +10,7 @@
 		   		<li><a href="#" class="fa fa-instagram mr-2"></a></li>
 		   		<li><a href="#" class="fa fa-envelope"></a></li>
 	    	</ul>
-			<div class="my_header_address"><a href="https://www.google.ca/maps/place/Daloussi+Auto/@49.8829714,-97.1118464,17z/data=!3m1!4b1!4m5!3m4!1s0x52ea76cd2dbc5f29:0xf42523ac8c67d856!8m2!3d49.882968!4d-97.1096577"><img src="img/gps_icon.png" alt="GPS" class="my_gps_icon">395 Goulet Street, Winnipeg, MB R2H 3C7</a></div>
+			<div class="my_header_address"><a href="https://www.google.ca/maps/place/Daloussi+Auto/@49.8829714,-97.1118464,17z/data=!3m1!4b1!4m5!3m4!1s0x52ea76cd2dbc5f29:0xf42523ac8c67d856!8m2!3d49.882968!4d-97.1096577" target="_blank"><img src="img/gps_icon.png" alt="GPS" class="my_gps_icon">395 Goulet Street, Winnipeg, MB R2H 3C7</a></div>
 			<div><span class="font-weight-bold mr-2 pl-3">Call Us: </span>(204) 583-4617</div>
 		</div>
 	</div>
--- php/main_nav.php
+++ php/main_nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid pb-0 pt-0">
+<nav class="navbar navbar-expand-md navbar-light bg-light container-fluid pb-0 pt-0 .navbar-fixed-top">
   <!-- <a class="navbar-brand" href="#">Home</a> -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
