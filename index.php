8e35771e798d0e8d5509d9e8ce81d807e51355c9
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-15 16:00:30 -0500

    new changes

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
             <?php include "php/about_us_home.php" ?>
         </div>
         <!-- Schedule Service: Usethis API https://github.com/n8barr/automotive-model-year-data for the car make and model. -->
+        <!-- form on image to use with the schedule service -->
+        <!-- use image modals when displaying the cars -->
         <footer>
             <?php include 'php/footer.php'; ?>
         </footer>
--- php/about_us_home.php
+++ php/about_us_home.php
@@ -1,18 +1,18 @@
-<div class="about_us_home container-fluid">
-	<div class="row justify-content-sm-center pl-5 pr-5">
+<div class="about_us_home container">
+	<div class="row justify-content-sm-center no-gutters">
 		<div class="col-sm-6">
 			<img class="img-fluid" src="img/dealership.jpg">
 		</div>
 		<div class="col-sm-6 pr-0">
 			<div class="row">
-				<div class="col-9 pt-3 bg-dark text-light pb-2">
+				<div class="col-9 pt-3 justify-content-sm-center bg-dark text-light pb-3">
 					<h1>Welcome To Daloussi Auto</h1>
 					<h4>Used Cars, SUVs, & Trucks in Winnipeg</h4>
 				</div>
 				<div class="col-sm-auto pl-0"></div>
 			</div>
-			<div class="row">
-				<div class="col-9 pt-3 bg-primary text-white">
+			<div class="row justify-content-sm-center">
+				<div class="col-9 pt-2 bg-primary text-white">
 					<p> Welcome to Daloussi Auto , We are the premier used car dealer for customers shopping for pre-owned cars, trucks, or SUV's in Manitoba .We have an inventory of vehicles that smaller dealers cannot match. No matter what make, model, year, or body type, we'll have the perfect car for your needs at a great price.</p>
 					<p>We stand out from other used car dealerships with our great service. Our finance department works hard every day to find you the best low rates and loan packages, and our zero-negotiation policy means that every customer will get the same low prices regardless of their bargaining power.</p>
 				</div>
