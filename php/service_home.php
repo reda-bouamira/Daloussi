5a85abee6acd27fae9d7645279ba92ead5b8a84a
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-26 14:58:39 -0500

    updated home_service

--- php/service_home.php
+++ php/service_home.php
@@ -1,45 +1,58 @@
 <div class="container-fluid my_service_home mt-0 pb-4">
-	<div class="row pl-5">
-		<h3 class="text-white">Schedule Service:</h3>
+	<div class="row justify-content-md-center py-5">
+		<div class="col-lg-3 my_about_service">
+			<h2 class="text-white pb-2">Schedule Service:</h2>
+			<p>If you’re looking for dealership that you can trust for maintenance, service or original parts for your vehicle, come to Daloussi Auto. Not only will you find competent technicians and cutting-edge facilities, but you will also get a level of customer service that ensures you’ll always leave our dealership with a smile.
+			</p>
+			<p>At Daloussi Auto, our technicians receive continuous training to ensure the quality of their work. Whether you need a simple oil change, routine maintenance or a more comprehensive repair, you can trust the service department.</p>
+			<p>For more information on the parts and services offered at Daloussi Auto, contact us today or make an appointment using our online contact form.</p>
+			</p>
+		</div>
+		<div class="col-lg-5 px-0 justify-content-md-center">
+			<form class="px-0">
+				<div class="form-row justify-content-md-center my_service_home_form_input">
+					<div class="form-group col-lg-5 form-group-lg mx-2">
+						<h6 class="p-0 m-0">Contact Info:</h6>
+			      		<label for="my_service_name">Name:</label>
+			      		<input type="text" class="form-control" id="my_service_name" placeholder="Full Name">
+			      		<label for="my_service_phone">Phone:</label>
+			      		<input type="tel" class="form-control" id="my_service_phone" placeholder="Phone Number">
+			      		<label for="my_service_email">Email</label>
+			      		<input type="email" class="form-control" id="my_service_email" placeholder="Email">
+			    	</div>
+			    </div>
+			    <div class="form-row justify-content-md-center my_service_home_form_input">
+			    	<div class="form-group col-lg-5 mx-2">
+			    		<h6 class="p-0 m-0">Appointment Info:</h6>
+						<div id="datetimepicker4" class="input-append">
+							<label for="my_service_date">Service Date:</label>
+							<input class="form-control form-control-lg" data-format="yyyy-MM-dd" type="text" id="my_service_date">
+							<span class="add-on">
+									<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+							</span>	
+						</div>
+						<label for="my_service_time">Service Time:</label>
+						<select id="my_service_time" class="form-control">
+							<option>Morning</option>
+							<option>Mid-day</option>
+							<option>Afternoon</option>
+							<option>Evening</option>
+						</select>
+			    	</div>
+			    </div>
+			    <div class="form-row justify-content-md-center my_service_home_form_input">
+			    	<div class="form-group col-lg-5 mx-2">
+			    		<h6 class="p-0 m-0">Vehicle Info:</h6>
+			    		<label for="my_service_car_year">Year:</label>
+						<select id="my_service_car_year" class="form-control">
+						</select>
+						<label for="my_service_make">Make:</label>
+			      		<input type="text" class="form-control" id="my_service_make">
+			      		<label for="my_service_model">Model:</label>
+			      		<input type="text" class="form-control" id="my_service_model">
+			    	</div>
+		    	</div>
+			</form>
+		</div>
 	</div>
-	<form class=" container justify-content-md-center">
-		<div class="form-row justify-content-md-center my_service_home_form_input">
-			<div class="form-group col-lg-2 form-group-lg mx-2">
-				<h6 class="p-0 m-0">Contact Info:</h6>
-	      		<label for="my_service_name">Name:</label>
-	      		<input type="text" class="form-control" id="my_service_name" placeholder="Full Name">
-	      		<label for="my_service_phone">Phone:</label>
-	      		<input type="tel" class="form-control" id="my_service_phone" placeholder="Phone Number">
-	      		<label for="my_service_email">Email</label>
-	      		<input type="email" class="form-control" id="my_service_email" placeholder="Email">
-	    	</div>
-	    	<div class="form-group col-lg-2 mx-2">
-	    		<h6 class="p-0 m-0">Appointment Info:</h6>
-				<div id="datetimepicker4" class="input-append">
-					<label for="my_service_date">Service Date:</label>
-					<input class="form-control form-control-lg" data-format="yyyy-MM-dd" type="text" id="my_service_date">
-					<span class="add-on">
-							<i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-					</span>	
-				</div>
-				<label for="my_service_time">Service Time:</label>
-				<select id="my_service_time" class="form-control">
-					<option>Morning</option>
-					<option>Mid-day</option>
-					<option>Afternoon</option>
-					<option>Evening</option>
-				</select>
-	    	</div>
-	    	<div class="form-group col-lg-2 mx-2">
-	    		<h6 class="p-0 m-0">Vehicle Info:</h6>
-	    		<label for="my_service_car_year">Year:</label>
-				<select id="my_service_car_year" class="form-control">
-				</select>
-				<label for="my_service_make">Make:</label>
-	      		<input type="text" class="form-control" id="my_service_make">
-	      		<label for="my_service_model">Model:</label>
-	      		<input type="text" class="form-control" id="my_service_model">
-	    	</div>
-    	</div>
-	</form>
 </div>
