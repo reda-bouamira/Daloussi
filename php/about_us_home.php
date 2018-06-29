82fbaac8dca0cb5a2686831778a9500e933d2ece
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-06-29 16:02:55 -0500

    Modified about_us

--- php/about_us_home.php
+++ php/about_us_home.php
@@ -1,4 +1,4 @@
-<div class="about_us_home container my_about_us_home px-0">
+<!-- <div class="about_us_home container my_about_us_home px-0">
 	<div class="row justify-content-sm-center no-gutters">
 		<div class="col-sm-6">
 			<img class="img-fluid my_dealer_img" src="img/dealership.jpg" alt="dealership">
@@ -14,6 +14,36 @@
 				<div class="col-12 pt-3 pb-3">
 					<p> Welcome to Daloussi Auto , We are the premier used car dealer for customers shopping for pre-owned cars, trucks, or SUV's in Manitoba .We have an inventory of vehicles that smaller dealers cannot match. No matter what make, model, year, or body type, we'll have the perfect car for your needs at a great price.</p>
 					<p>We stand out from other used car dealerships with our great service. Our finance department works hard every day to find you the best low rates and loan packages, and our zero-negotiation policy means that every customer will get the same low prices regardless of their bargaining power.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> -->
+
+<div class="about_us_home container-fluid my_about_us_home pt-5 px-0">
+	<div class="row justify-content-sm-center">
+		<div class="col-sm-9 bg-white about_us_home_content px-0">
+			<div class="row justify-content-sm-center no-gutters">
+				<div class="col">
+					<img class="img-fluid my_dealer_img" src="img/dealership.jpg" alt="dealership">
+				</div>
+			</div>
+			<div class="row justify-content-sm-center mx-0">
+				<div class="row ml-0">
+					<div class="col-sm pt-4 justify-content-sm-center pb-3 my_about_us_home_header bg-white">
+						<center>
+							<h1>Welcome To Daloussi Auto</h1>
+							<h4>Used Cars, SUVs, & Trucks in Winnipeg</h4>
+						</center>
+					</div>
+				</div>
+				<div class="row justify-content-sm-center mx-0 ml-0 my_about_us_home_text">
+					<div class="col-sm-12 mx-0 px-0 pt-0 pb-3">
+						<center class="bg-white">
+							<p> Welcome to Daloussi Auto , We are the premier used car dealer for customers shopping for pre-owned cars, trucks, or SUV's in Manitoba. We have an inventory of vehicles that smaller dealers cannot match. No matter what make, model, year, or body type, we'll have the perfect car for your needs at a great price.</p>
+							<p>We stand out from other used car dealerships with our great service. Our finance department works hard every day to find you the best low rates and loan packages, and our zero-negotiation policy means that every customer will get the same low prices regardless of their bargaining power.</p>
+						</center>
+					</div>
 				</div>
 			</div>
 		</div>
