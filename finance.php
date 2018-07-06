0dd531aaf57fd719fe92454475b5209612cdbc54
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-07-06 15:54:22 -0500

    Working on the finance form

--- finance.php
+++ finance.php
@@ -12,7 +12,7 @@
                 <h2>Finance</h2>
             </div>
             <div class="row justify-content-center">
-                <form class="needs-validation jumbotron col-9 pt-3">
+                <form class="needs-validation jumbotron col-9 pt-3" id="finance_form">
                     <center><h3 class="pb-4">Application Form</h3></center>
                     <div class="my_personal_info">
                         <h5>Personal Information</h5>
@@ -20,7 +20,7 @@
                             <div class="form-row">
                                 <div class="form-group col-lg-7">
                                     <label for="fname">First Name*</label>
-                                    <input type="text" class="form-control" id="fname" required>
+                                    <input type="text" class="form-control" id="fname" name="fname" required>
                                 </div>
                                 <div class="form-group col-lg-5">
                                     <label for="sname">Middle Name</label>
@@ -70,11 +70,11 @@
                                 </div>
                                 <div class="form-group col-lg-4">
                                     <label for="h_phone">Home Phone</label>
-                                    <input type="tel" class="form-control" id="h_phone" required>
+                                    <input type="tel" class="form-control" id="h_phone" placeholder="(XXX) XXX-XXXX" maxlength="14" required>
                                 </div>
                                 <div class="form-group col-lg-4">
                                     <label for="c_phone">Cell Phone*</label>
-                                    <input type="tel" class="form-control" id="c_phone" required>
+                                    <input type="tel" class="form-control" id="c_phone" placeholder="(XXX) XXX-XXXX" maxlength="14" required>
                                 </div>
                             </div>
                         </div>
@@ -287,20 +287,121 @@
                             </div>
                         </div>
                         <div class="my_financial_details mt-4">
+                            <h5>Financial Details</h5>
                             <div class="form-row">
-                                <div class="form-group">
-                                    <label></label>
+                                <div class="form-group col-lg-3">
+                                    <label for="gross_monthly_income">Gross Monthly Income*</label>
+                                    <input id="gross_monthly_income" type="number" class="form-control" min="0">
                                 </div>
-                                <div class="form-group">
-                                    
+                                <div class="form-group col-lg-3">
+                                    <label for="other_monthly_income">Other Monthly Income*</label>
+                                    <input id="other_monthly_income" type="number" class="form-control" min="0">
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label for="other_monthly_income_desc">Description of Other</label>
+                                    <input id="other_monthly_income_desc" type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-lg-8">
+                                    <label for="total_monthly_income">Total Monthly Income*</label>
+                                    <input id="total_monthly_income" type="number" class="form-control" min="0" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="my_financial_obligations mt-4">
+                            <h5>Financial Obligations</h5>
+                            <div class="form-row">
+                                <div class="form-group col-lg-2">
+                                    <label for="home_status">Home Status*</label>
+                                    <select id="home_status" class="form-control">
+                                        <option selected>Renting</option>
+                                        <option>Owning</option>
+                                        <option>Other</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-lg-3">
+                                    <label for="landl_mort_holder">Landlord/Mortgage Holder</label>
+                                    <input id="landl_mort_holder" type="text" class="form-control">
+                                </div>
+                                <div class="form-group col-lg-3">
+                                    <label for="monthly_payment">Monthly Payment*</label>
+                                    <input id="monthly_payment" type="number" class="form-control" min="0" step="10" required>
+                                </div>
+                                <div class="form-group col-lg-3">
+                                    <label for="market_value">Market value</label>
+                                    <input id="market_value" type="number" class="form-control" min="0" step="1000">
+                                </div>
+                                <div class="form-group col-lg-5">
+                                    <label for="mort_balance">Mortgage Balance</label>
+                                    <input id="mort_balance" type="number" class="form-control" min="0" step="1000">
+                                </div>
+                                <div class="form-group col-lg-5">
+                                    <label for="mort_amount">Original Mortgage Amount</label>
+                                    <input id="mort_amount" type="number" class="form-control" min="0" step="1000">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="my_yes_no_aggreements">
+                            <div class="form-row">
+                                <div class="form-group col-lg-4">
+                                    <div class="text-danger">Have you claimed bankruptcy in the last 7 years?*</div>
+                                </div>
+                                <div class="form-group col-lg-2">
+                                    <label class="radio-inline pr-2">
+                                        <input class="mr-1" type="radio" name="optradio">Yes
+                                    </label>
+                                    <label class="radio-inline">
+                                        <input class="mr-1" type="radio" name="optradio">No
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-lg-4">
+                                    <div class="text-danger">Are you currently past due on payments?*</div>
+                                </div>    
+                                <div class="form-group col-lg-2">
+                                    <label class="radio-inline pr-2">
+                                        <input class="mr-1" type="radio" name="optradio">Yes
+                                    </label>
+                                    <label class="radio-inline">
+                                        <input class="mr-1" type="radio" name="optradio">No
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-lg-4">
+                                    <div class="text-danger">Do you have any collection outstanding?*</div>
+                                </div>    
+                                <div class="form-group col-lg-2">
+                                    <label class="radio-inline pr-2">
+                                        <input class="mr-1" type="radio" name="optradio">Yes
+                                    </label>
+                                    <label class="radio-inline">
+                                        <input class="mr-1" type="radio" name="optradio">No
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-lg-12">
+                                    <div class="text-dark">By accepting below means that all the information I have provided is to the best of my knowledge truthful. I hereby authorize EPIC Dealer Solutions Ltd. and credit lenders to obtain and exchange and release my credit report, personal information, and make any necessary inquiries regarding our loan application for the purpose of interested lenders providing funds on our behalf for our conditional vehicle purchase. I authorize those lending institutions who approve this loan application and are willing to provide funds on our behalf to forward a copy of the final loan agreement and all other relevant documents to EPIC Dealer Solutions or to the automobile dealership listed on page one of this application. </div>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="form-group">
-                                    
+                                    <label class="checkbox-inline"><input class="mr-2" type="checkbox" value="accept_aggreement">I accept*</label>
                                 </div>
-                                <div class="form-group">
-                                    
+                            </div>
+                        </div>
+                        <div class="my_submit">
+                            <div class="form-row">
+                                <div class="form-group col-lg-12">
+                                    <div>Please double check your information before proceeding. Once you submit the form, we will respond to your request as soon as possible.</div>
+                                </div>
+                            </div>
+                            <div class="form-row justify-content-end">
+                                <div class="form-group col-lg-2">
+                                    <button type="submit" class="btn btn-primary btn-lg">Submit</button>
                                 </div>
                             </div>
                         </div>
@@ -312,5 +413,8 @@
             <?php include 'php/footer.php'; ?>
         </footer>
     	<?php include 'php/scripts.php' ?>
+        <script type="text/javascript" src="js/finance.js"></script>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js'></script>
+<script>
     </body>
 </html>
