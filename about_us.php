365c30e16a5b7014ea9ff90d5f5b4792bf1f0a6f
reda-bouamira <33232054+reda-bouamira@users.noreply.github.com>
2018-08-01 08:11:30 -0500

    about us padding fixed

--- about_us.php
+++ about_us.php
@@ -21,16 +21,16 @@
 						</div>
 						<div class="row justify-content-sm-center mx-0">
 							<div class="row ml-0">
-								<div class="about_us_headers col-sm pt-4 justify-content-sm-center pb-3 my_about_us_home_header bg-white">
-									<center>
+								<div class="col-sm pt-4 justify-content-sm-center pb-3 my_about_us_home_header bg-white px-2 mx-1">
+									<center class="mx-2 px-2">
 										<h1>Welcome To Daloussi Auto</h1>
 										<h4>Used Cars, SUVs, & Trucks in Winnipeg</h4>
 									</center>
 								</div>
 							</div>
-							<div class="row justify-content-sm-center mx-0 ml-0 text-secondary">
-								<div class="col-sm-12 mx-0 px-0 pt-0 pb-5 mb-5">
-									<center class="bg-white">
+							<div class="row justify-content-sm-center mx-0 ml-0 my_about_us_home_text">
+								<div class="col-sm-12 mx-0 px-0 pt-0 pb-3">
+									<center class="bg-white mx-2 px-2">
 										<p> Welcome to Daloussi Auto , We are the premier used car dealer for customers shopping for pre-owned cars, trucks, or SUV's in Manitoba. We have an inventory of vehicles that smaller dealers cannot match. No matter what make, model, year, or body type, we'll have the perfect car for your needs at a great price.</p>
 										<p>We stand out from other used car dealerships with our great service. Our finance department works hard every day to find you the best low rates and loan packages, and our zero-negotiation policy means that every customer will get the same low prices regardless of their bargaining power.</p>
 									</center>
